7a8f921e465666e978d2f0e9f53f85bd4d41eff5
abhinav rana <abhinav.rana1990@gmail.com>
2016-07-30 15:51:58 +0530

    Redirection code added using event subscriber

---
+++ src/EventSubscriber/DomainRedirectEventSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\domain_301_redirect\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+/**
+ * Class DomainRedirectEventSubscriber.
+ *
+ * @package Drupal\domain_301_redirect
+ */
+class DomainRedirectEventSubscriber implements EventSubscriberInterface {
+
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    $events['kernel.request'] = ['requestHandler'];
+
+    return $events;
+  }
+
+  /**
+   * This method is called whenever the kernel.request event is
+   * dispatched.
+   *
+   * @param GetResponseEvent $event
+   */
+  public function requestHandler(GetResponseEvent $event) {
+     $response = new RedirectResponse("http://www.google.com",301);
+     $response->send();
+  }
+
+}
